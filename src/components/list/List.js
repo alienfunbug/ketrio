9ce6d252afd958177c15d3df61cb1448605de79f
alienfunbug <71366134+alienfunbug@users.noreply.github.com>
2021-08-23 20:58:35 -0700

    sliders functionality in progress

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,30 +1,62 @@
 import "./List.css";
 import ListItem from "../listItem/ListItem";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { ArrowBackIosOutlined, ArrowForwardIos } from "@material-ui/icons";
 
 function List() {
+  
+  const [isMoved, setIsMoved] = useState(false);
+  const listReference = useRef();
+  const tileWidthLeft = 215;
+  const tileWidthRight = 255;
+  //tile width - 10px for margin right of listItem
+
+  const clickHandler = (direction) => {
+    setIsMoved(true); //activate slider
+
+    let distance = listReference.current.getBoundingClientRect().x - 50;
+
+    if (direction === "left") {
+      listReference.current.style.transform = `translateX(${
+        tileWidthLeft + distance
+      }px)`;
+      console.log(distance);
+    }
+
+    if (direction === "right") {
+      listReference.current.style.transform = `translateX(${
+        tileWidthRight * -1 + distance
+      }px)`;
+      console.log(distance);
+    }
+  };
+
   return (
     <div className="list-container">
       <div className="title">All Stars</div>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="slider-arrow-left"/>
-        <div className="container">
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-
-
-
+        <ArrowBackIosOutlined
+          className="slider-arrow-left"
+          onClick={() => clickHandler("left")}
+          //style={{display: !isMoved && "none"}} slider
+        
+        />
+        <div className="container" ref={listReference}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
         </div>
-        <ArrowForwardIos className="slider-arrow-right"/>
+        <ArrowForwardIos
+          className="slider-arrow-right"
+          onClick={() => clickHandler("right")}
+        />
       </div>
     </div>
   );
