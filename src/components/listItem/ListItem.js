251680ad150a2b8eb32bbec579d2df0ce02fe6aa
alienfunbug <71366134+alienfunbug@users.noreply.github.com>
2021-08-24 13:22:38 -0700

    hover effect on container, mock img effect

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -1,13 +1,31 @@
+import { PlayArrow, Add, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@material-ui/icons";
 import React from "react";
 import "./ListItem.css";
 
-function ListItem (){
-
-    return(
-        <div className="listItem">
-            Place holder
+function ListItem() {
+  return (
+    <div className="listItem">
+      <img src="../assets/images/Eric_full.PNG" 
+      alt="" 
+      />
+      <div className="itemInfo">
+        <div className="icons">
+            ELKE
+            <PlayArrow/>
+            <Add/>
+            <ThumbUpAltOutlined/>
+            <ThumbDownAltOutlined/>
+           
         </div>
-    )
+        <div className="itemInfoTop">
+
+            <span>SF</span>
+            <span>PG</span>
+            <span>SG</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ListItem;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,7 @@ function HomePage() {
             <List/>
             <List/>
             <List/>
+            <List/>
         </div>)
 }
 
