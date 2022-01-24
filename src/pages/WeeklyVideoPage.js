403da1e1702ec00b6e627c844b8ffc7f0ca3d5d3
alienfunbug <71366134+alienfunbug@users.noreply.github.com>
2022-01-23 20:10:34 -0700

    updated weekly video

--- src/pages/WeeklyVideoPage.js
+++ src/pages/WeeklyVideoPage.js
@@ -7,7 +7,7 @@ function WeeklyVideoPage() {
 
 
   useEffect(() => {
-    window.location.href = "https://www.youtube.com/watch?v=3KX2I5iCWpI";
+    window.location.href = "https://www.youtube.com/watch?v=AgCrGsZEfmg";
   }, []);
 
   return (
