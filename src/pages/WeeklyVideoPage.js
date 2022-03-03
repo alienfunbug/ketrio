2a8d4c9379159f20ebd0cb6e5c837a1663e3cc86
alienfunbug <71366134+alienfunbug@users.noreply.github.com>
2022-03-02 17:04:43 -0700

    fix motizuki, update selena char,update weekly

--- src/pages/WeeklyVideoPage.js
+++ src/pages/WeeklyVideoPage.js
@@ -7,7 +7,7 @@ function WeeklyVideoPage() {
 
 
   useEffect(() => {
-    window.location.href = "https://www.youtube.com/watch?v=VFXrJvgjOQY";
+    window.location.href = "https://www.youtube.com/watch?v=Z1Q4pJEJVAo";
   }, []);
 
   return (
