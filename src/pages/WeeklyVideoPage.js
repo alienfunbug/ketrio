92d78be0a42d36cbf65749c8f01bb201b9b0c30e
alienfunbug <71366134+alienfunbug@users.noreply.github.com>
2022-06-29 21:25:35 -0700

    update luka

--- src/pages/WeeklyVideoPage.js
+++ src/pages/WeeklyVideoPage.js
@@ -7,7 +7,7 @@ function WeeklyVideoPage() {
 
 
   useEffect(() => {
-    window.location.href = "https://www.youtube.com/watch?v=72Fn9tIcui8";
+    window.location.href = "https://www.youtube.com/watch?v=KPWT5yI0USw";
   }, []);
 
   return (
